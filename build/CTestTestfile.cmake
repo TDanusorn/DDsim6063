8ea0a23867fb921719618b2f2a03f401f98fc8af
Danusorn <torcen733@gmail.com>
2021-05-23 14:23:34 +0700

    commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/leng/tide/DDsim6063/src
+# Build directory: /home/leng/tide/DDsim6063/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("dd_sim")
---
+++ build/dd_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leng/tide/DDsim6063/src/dd_sim
+# Build directory: /home/leng/tide/DDsim6063/build/dd_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
